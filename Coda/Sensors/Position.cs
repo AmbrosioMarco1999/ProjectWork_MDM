7d5d3544d30a4c58d85e788e37200c9763e0a3eb
Marco Ambrosio <marco.ambrosio@tecnicosuperiorekennedy.com>
2019-04-16 10:16:51 +0200

    Raw File

---
+++ Coda/Sensors/Position.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Coda.Sensors
+{
+    class Position
+    {
+
+        double _latitudine;
+        double _longitudine;
+        Position()
+        {
+
+        }
+
+        Position(double latitudine, double longitudine)
+        {
+            _latitudine = latitudine;
+            _longitudine = longitudine;
+        }
+
+        public double Longitudine { get => _longitudine; set => _longitudine = value; }
+        public double Latitudine { get => _latitudine; set => _latitudine = value; }
+
+        private double GetRandomPosition()
+
+
+        public string GetPositionToJSON()
+        {
+            return "{\"latitude\":\"" + Latitudine + "\"" + "," + "\"temperature\":\"" + Longitudine + "\"}";
+        }
+    }
+}
